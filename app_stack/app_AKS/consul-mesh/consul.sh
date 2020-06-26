52477c3bc8d4cdc15dc3f723e23b1df20fdd8ef7
dawright22 <53329137+dawright22@users.noreply.github.com>
2020-06-26 15:26:16 +1000

    Add files via upload

---
+++ app_stack/app_AKS/consul-mesh/consul.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+set -v
+
+
+helm3 repo add hashicorp https://helm.releases.hashicorp.com
+
+echo "applying secret for federation CA and mesh gateway address..."
+kubectl apply -f ../../app_GKE/consul-mesh/consul-federation-secret.yaml
+
+echo "Installing consul using latest helm chart "
+helm install consul hashicorp/consul -f values.yaml #--debug
+
+sleep 10s
+
+kubectl apply -f customdns.yaml
+
+sleep 2
+
+kubectl delete pod --namespace kube-system -l k8s-app=kube-dns
