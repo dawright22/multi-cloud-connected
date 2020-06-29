f5b5d1bbf119833b20d17baab64c94c512230c5c
dawright22 <53329137+dawright22@users.noreply.github.com>
2020-06-29 15:53:12 +1000

    Update varables.tf

--- Cluster_AKS/varables.tf
+++ Cluster_AKS/varables.tf
@@ -2,11 +2,11 @@ variable "cluster-name" {
 }
 
 variable "appId" {
-  default = "4dc3caf3-78a4-4809-a023-42574206c3d0"
+  default = ""
 }
 
 variable "password" {
-  default = "c6f3d74a-1741-49b8-a9a0-9731d8673ce2"
+  default = ""
 }
 
 variable "location" {
